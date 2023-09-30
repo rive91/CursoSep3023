4062288b3904845952fc1eb2fe93f1f573d4b225
rive <rive@MSI>
2023-09-30 11:07:24 -0600

    Primer Commit

---
+++ CoolTesters923/src/test/java/pruebaSelenium/NewTest.java
@@ -0,0 +1,13 @@
+package pruebaSelenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.google.com");
+  }
+}
