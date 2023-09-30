726499c375d0c949e5812829e08c3b229a65a644
rive <rive@MSI>
2023-09-30 11:36:33 -0600

    Cambio de URL

--- CoolTesters923/src/main/java/practicaSel/PruebaSelenium.java
+++ CoolTesters923/src/main/java/practicaSel/PruebaSelenium.java
@@ -7,7 +7,7 @@ public class PruebaSelenium {
 
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com");
+		driver.get("https://www.cooltesers.com");
 
 	}
 
